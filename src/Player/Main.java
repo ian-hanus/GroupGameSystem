34fe83385119bf22ec70f7567031fa95a72a0383
pavelpivarshev <pap27@duke.edu>
2019-03-23 18:43:29 -0400

    beginning Main and PlayerStage

--- src/Player/Main.java
+++ src/Player/Main.java
@@ -3,6 +3,10 @@ package Player;
 public class Main extends Application {
 
     public void start(Stage stage) {
+        
+        PlayerStage player = new PlayerStage();
+        Stage st = player.makeStage();
+        st.show();
 
     }
 
