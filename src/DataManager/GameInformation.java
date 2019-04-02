9945b43b36d4c99dc8a8ca0aedf3a528db0b33fa
Ian Hanus <ih52@duke.edu>
2019-04-01 22:24:12 -0400

    Added outline for GameInformation and Level classes

--- src/DataManager/GameInformation.java
+++ src/DataManager/GameInformation.java
@@ -7,10 +7,12 @@ public class GameInformation {
     private List<Level> myLevels;
     private Map<String, Integer> myScoreboard;
     private Splash mySplash;
+    private Map<List<GameObject>, String> myInteractions;
 
-    public GameInformation(List<Level> levels, Map<String, Integer> scoreboard, Splash splash){
+    public GameInformation(List<Level> levels, Map<String, Integer> scoreboard, Splash splash, Map<List<GameObject>, String> interactions){
         myLevels = levels;
         myScoreboard = scoreboard;
         mySplash = splash;
+        myInteractions = interactions;
     }
 }
--- src/DataManager/Level.java
+++ src/DataManager/Level.java
@@ -1,4 +1,13 @@
 package DataManager;
 
 public class Level {
+    List<GameObject> myGameObjects;
+    Integer myLevelNumber;
+    List<GameObject> myObjectBank;
+
+    public Level(Integer levelNumber, List<GameObject> gameObjects, List<GameObject> objectBank){
+        myLevelNumber = levelNumber;
+        myGameObjects = gameObjects;
+        myObjectBank = objectBank;
+    }
 }
