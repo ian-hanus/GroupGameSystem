72a401ce14242e240c506044dde8147305dacdc4
Jonathan Yu <jy178@duke.edu>
2019-04-04 08:31:40 -0400

    Fix package statement and add powerup functionality in User class

--- src/Engine/src/GameObjects/User.java
+++ src/Engine/src/GameObjects/User.java
@@ -1,8 +1,28 @@
-package GameObjects;
+package Engine.src.GameObjects;
 
+import Powerups.Powerup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jonathan Yu
+ */
 public class User extends GameObject {
+
+    List<Powerup> activePowerups;
+
     public User(double xPos, double yPos, double health, double height, double width,
-                      double angle, String objectName, int zIndex, double[] direction, String filename) {
+                double angle, String objectName, int zIndex, double[] direction, String filename) {
         super(xPos, yPos, health, height, width, angle, objectName, zIndex, direction, filename);
+        activePowerups = new ArrayList<>();
+    }
+
+    public void addPowerup(Powerup powerup) {
+        activePowerups.add(powerup);
+    }
+
+    public List<Powerup> getActivePowerups() {
+        return activePowerups;
     }
 }
