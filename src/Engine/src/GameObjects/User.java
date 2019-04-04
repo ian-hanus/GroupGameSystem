6c0e23104b38d1fa37e9c9973680660df1d5d19b
Jonathan Yu <jy178@duke.edu>
2019-04-04 09:43:21 -0400

    Move activePowerups initialization out of constructor in User class

--- src/Engine/src/GameObjects/User.java
+++ src/Engine/src/GameObjects/User.java
@@ -10,12 +10,11 @@ import java.util.List;
  */
 public class User extends Character {
 
-    private List<Powerup> activePowerups;
+    private List<Powerup> activePowerups = new ArrayList<>();
 
     public User(double xPos, double yPos, double health, double height, double width,
                 double angle, String objectName, int zIndex, double[] direction, String filename, double jumpSpeed) {
         super(xPos, yPos, health, height, width, angle, objectName, zIndex, direction, filename, jumpSpeed);
-        activePowerups = new ArrayList<>();
     }
 
     public void addPowerup(Powerup powerup) {
