f824236463e9c363440aa932cb007ff63a4b2a4d
Jonathan Yu <jy178@duke.edu>
2019-04-04 08:45:58 -0400

    Add Character class and its jump functionality

--- src/Engine/src/GameObjects/Character.java
+++ src/Engine/src/GameObjects/Character.java
@@ -1,4 +1,19 @@
 package Engine.src.GameObjects;
 
-public class Character {
+public class Character extends GameObject {
+
+    double myJumpSpeed;
+
+    public Character(double xPos, double yPos, double health, double height, double width,
+                double angle, String objectName, int zIndex, double[] direction, String filename, double jumpSpeed) {
+        super(xPos, yPos, health, height, width, angle, objectName, zIndex, direction, filename);
+        myJumpSpeed = jumpSpeed;
+    }
+
+    public void jump() {
+        double[] nextDirection = getDirection();
+
+        nextDirection[1] = nextDirection[1] + myJumpSpeed;
+        setDirection(nextDirection);
+    }
 }
