3ce922c5f59b56dad4eb87b875db371876934ad3
Ian Hanus <ih52@duke.edu>
2019-04-04 15:07:43 -0400

    Got rid of simple json dependency and added the requisity gson jar file to library

--- src/DataManager/DataSaver.java
+++ src/DataManager/DataSaver.java
@@ -2,7 +2,6 @@ package DataManager;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.json.simple.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -12,7 +11,7 @@ public class DataSaver {
     public DataSaver(String fileName, Collection<Object> objects){
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
-            gson.toJson(objects, new FileWriter(fileName));
+            gson.toJson(objects, new FileWriter("./" + fileName));
         } catch (IOException e) {
             System.out.println("Unable to save game to JSON file.");
         }
