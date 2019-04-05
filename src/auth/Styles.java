b61bd1eab521ef05513a08f19fa530defb3b68bc
Anshu Dwibhashi <ad353@duke.edu>
2019-04-05 14:18:46 -0400

    Refactored panes into a class hierarchy.

--- src/auth/Styles.java
+++ src/auth/Styles.java
@@ -2,11 +2,11 @@ package auth;
 
 public class Styles {
     public static String BG_STYLE = "-fx-background-color : #808080;";
-    public static String TOOLS_PANE_STYLE = "-fx-background-color : #d8d8d8;" +
+    public static String LEFT_PANE_STYLE = "-fx-background-color : #d8d8d8;" +
             "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.5), 10, 0, 0, 0);" +
             "-fx-border-radius: 0 15 15 0;" +
             "-fx-background-radius: 0 15 15 0;";
-    public static String PROPS_PANE_STYLE = "-fx-background-color : #d8d8d8;" +
+    public static String RIGHT_PANE_STYLE = "-fx-background-color : #d8d8d8;" +
             "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.5), 10, 0, 0, 0);" +
             "-fx-border-radius: 15 0 0 15;" +
             "-fx-background-radius: 15 0 0 15;";
--- src/auth/helpers/ScreenHelpers.java
+++ src/auth/helpers/ScreenHelpers.java
@@ -1,32 +1,35 @@
 package auth.helpers;
 
+import auth.panes.LeftPane;
+import auth.panes.RightPane;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
 import static auth.Dimensions.*;
 import static auth.Styles.*;
 import static auth.helpers.DimensionCalculator.*;
-import static auth.helpers.RectangleHelpers.*;
 
 public class ScreenHelpers {
     public static void initScene(Scene scene, Group root) {
         scene.setRoot(root);
         root.setStyle(BG_STYLE);
-        placeBackgroundRectangles(root);
+        placePanes(root);
     }
 
-    private static void placeBackgroundRectangles(Group root) {
-        var toolsPane = createStyledRectangle(LEFT_EDGE, centreVertical(TOOLS_PANE_HEIGHT),
-                TOOLS_PANE_WIDTH, TOOLS_PANE_HEIGHT, TOOLS_PANE_STYLE);
-        var propsPane = createStyledRectangle(computeRightEdge(RIGHT_PANE_WIDTH), TOP_EDGE,
-                RIGHT_PANE_WIDTH, RIGHT_PANE_HEIGHT, PROPS_PANE_STYLE);
-        var objLibPane = createStyledRectangle(computeRightEdge(RIGHT_PANE_WIDTH),
-                computeMarginToBottomEdge(propsPane, RIGHT_PANE_MARGIN),
-                RIGHT_PANE_WIDTH, RIGHT_PANE_HEIGHT, PROPS_PANE_STYLE);
+    private static void placePanes(Group root) {
+        var toolsPane = new LeftPane(centreVertical(TOOLS_PANE_HEIGHT), TOOLS_PANE_WIDTH, TOOLS_PANE_HEIGHT);
+        var propsPane = new RightPane(TOP_EDGE, RIGHT_PANE_WIDTH, RIGHT_PANE_HEIGHT);
+        var objLibPane = new RightPane(computeMarginToBottomEdge(propsPane.getView(), RIGHT_PANE_MARGIN), RIGHT_PANE_WIDTH, RIGHT_PANE_HEIGHT);
 
-        Group rightPanes = new Group();
-        rightPanes.getChildren().addAll(propsPane, objLibPane);
-        rightPanes.setLayoutY(centreVertical(rightPanes.getLayoutBounds().getHeight()));
-        root.getChildren().addAll(toolsPane, rightPanes);
+        var rightPanesGroup = new Group(propsPane.getView(), objLibPane.getView());
+        rightPanesGroup.setLayoutY(centreVertical(rightPanesGroup.getLayoutBounds().getHeight()));
+
+        //var consolePane = new ConsolePane();
+        //var canvasPane = new CanvasPane();
+        root.getChildren().addAll(toolsPane.getView(),
+                rightPanesGroup);
+                //consolePane.getView(),
+                //canvasPane.getView());
     }
+
 }
