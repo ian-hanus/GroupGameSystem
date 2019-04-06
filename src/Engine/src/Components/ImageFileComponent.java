98858c7bc1e2aad12b27d9f6c9a2e075ab4807c6
David Liu <dwl23@duke.edu>
2019-04-06 16:58:38 -0400

    Started ECS Design by making Entity Interface and Components

---
+++ src/Engine/src/Components/ImageFileComponent.java
@@ -0,0 +1,13 @@
+package Components;
+
+import java.io.File;
+
+public class ImageFileComponent {
+    //private FilePath for set of files
+    private File currentFile;
+
+
+    public ImageFileComponent(File file) {
+        currentFile = file;
+    }
+}
