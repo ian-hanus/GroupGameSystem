f8244f7126f2072cf6831e568ff533d2d6d3f084
Hunter Gregory <hlg16@duke.edu>
2019-04-06 21:33:40 -0400

    Created the entity class with only an id getter #39

---
+++ src/Engine/src/Entities/Entity.java
@@ -0,0 +1,13 @@
+package Entities;
+
+public class Entity {
+    private int myID;
+
+    public Entity(int id) {
+        myID = id;
+    }
+
+    public int getID() {
+        return myID;
+    }
+}
