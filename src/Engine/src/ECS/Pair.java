17729f8f550029acee67c4e422d37a64932c1ad7
Hunter Gregory <hlg16@duke.edu>
2019-04-07 21:15:55 -0400

    made pair iterable

--- src/Engine/src/ECS/Pair.java
+++ src/Engine/src/ECS/Pair.java
@@ -1,31 +1,51 @@
 package ECS;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 /**
- * An ordered pair with a robust check for equality.
+ * An iterable, ordered pair with a robust check for equality.
  */
-public class Pair<T> {
-    T myItem1;
-    T myItem2;
+public class Pair<T> implements Iterable<T> {
+    ArrayList<T> myItems;
 
     public Pair(T item1, T item2) {
-        myItem1 = item1;
-        myItem2 = item2;
+        myItems = new ArrayList<>();
+        myItems.add(item1);
+        myItems.add(item2);
     }
 
-    public T getFirstTag() {
-        return myItem1;
+    public T getItem1() {
+        return myItems.get(0);
     }
 
-    public T getSecondTag() {
-        return myItem2;
+    public T getItem2() {
+        return myItems.get(0);
     }
 
     @Override
     public boolean equals(Object other) {
         if (this.getClass().isInstance(other)) {
             var otherPair = (Pair<T>) other;
-            return myItem1.equals(otherPair.myItem1) && myItem2.equals(otherPair.myItem2);
+            return getItem1().equals(otherPair.getItem1()) && getItem2().equals(otherPair.getItem2());
         }
         return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return myItems.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        myItems.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return myItems.spliterator();
     }
 }
