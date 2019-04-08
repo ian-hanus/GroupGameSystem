0ac2c61f2ae80ed3197ad51883be3cbc3d113b89
Hunter Gregory <hlg16@duke.edu>
2019-04-08 19:28:52 -0400

    #42 moved the impassable right left bottom and top components to one component

--- src/Engine/src/ECS/Components/ImpassableLeftComponent.java
+++
@@ -1,9 +0,0 @@
-package ECS.Components;
-
-public class ImpassableLeftComponent extends Component {
-    private Boolean myImpassableLeft;
-
-    public ImpassableLeftComponent(Boolean impassableLeft) {
-        myImpassableLeft = impassableLeft;
-    }
-}
