9ac683c08d68183827a6c410e2188fc2b1d84419
Ian Hanus <ih52@duke.edu>
2019-04-07 23:55:54 -0400

    Object bank images now working and updating correctly and Components are now read in from properties file

--- src/auth/ObjectControl/ObjectManager.java
+++ src/auth/ObjectControl/ObjectManager.java
@@ -1,5 +1,6 @@
 package auth.ObjectControl;
 
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import java.util.HashMap;
@@ -9,33 +10,42 @@ import java.util.Set;
 public class ObjectManager {
     private ObjectCreator myObjectCreator;
     private ObjectBank myObjectBank;
-    private Map<Integer, Map<String, Integer>> myUniqueObjects;
-    private Map<Integer, Map<String, Integer>> myObjects;
+    private Map<Integer, Map<String, String>> myUniqueObjects;
+    private Map<Integer, Map<String, String>> myObjects;
     private Map<String[], Set<String>> myCollisions;
     private Integer myObjectCount = 0;
     private Integer myUniqueObjectCount = 0;
+    private Pane myPane;
 
-    public ObjectManager(){
+    public ObjectManager(Pane pane){
         myObjectCreator = new ObjectCreator(this);
         myObjectBank = new ObjectBank(this);
         myUniqueObjects = new HashMap<>();
         myObjects = new HashMap<>();
         myCollisions = new HashMap<>();
+        myPane = pane;
     }
 
     public VBox getCreatorView(){
         return myObjectCreator.getView();
     }
 
-    public void writeUniqueObject(Map<String, Integer> objectComponents){
+    public VBox getBankView(){
+        return myObjectBank.getView();
+    }
+
+    public void writeUniqueObject(Map<String, String> objectComponents){
         myUniqueObjects.put(myUniqueObjectCount, objectComponents);
         myUniqueObjectCount++;
+        myObjectBank.addObject(objectComponents);
+        myPane.getChildren().removeAll();
+        myPane.getChildren().add(myObjectBank.getView());
     }
 
     // TODO: add the x and y components to the map before passing to DataManager
-    public void writeObject(Map<String, Integer> objectComponents, double xLoc, double yLoc){
-        objectComponents.put("xLoc", (int) xLoc);
-        objectComponents.put("yLoc", (int) yLoc);
+    public void writeObject(Map<String, String> objectComponents, double xLoc, double yLoc){
+        objectComponents.put("xLoc", Double.toString(xLoc));
+        objectComponents.put("yLoc", Double.toString(yLoc));
         myObjects.put(myObjectCount, objectComponents);
         myObjectCount++;
     }
