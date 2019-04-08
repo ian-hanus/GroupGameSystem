91d532b7ea6ace801f734a240a9af17d59b8383d
David Liu <dwl23@duke.edu>
2019-04-08 16:31:19 -0400

    Impassable components

---
+++ src/Engine/src/ECS/Components/ImpassableBottomComponent.java
@@ -0,0 +1,9 @@
+package ECS.Components;
+
+public class ImpassableBottomComponent extends Component {
+    private Boolean myImpassableBottom;
+
+    public ImpassableBottomComponent(Boolean impassableBottom) {
+        myImpassableBottom = impassableBottom;
+    }
+}
