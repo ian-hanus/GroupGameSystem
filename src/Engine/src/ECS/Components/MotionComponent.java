602ad570d210cc14abca949d41173b114c8065e3
Hunter Gregory <hlg16@duke.edu>
2019-04-07 22:20:16 -0400

    made Motion component have x and y components of accel and vel. Eliminates need to use sines and cosines in physics. Revert this commit to go back to the direction vector model (will need to include a direction vector for both acceleration and velocity

--- src/Engine/src/ECS/Components/MotionComponent.java
+++ src/Engine/src/ECS/Components/MotionComponent.java
@@ -1,47 +1,57 @@
 package ECS.Components;
 
 public class MotionComponent extends Component {
-    private double myVel;
-    private double myAcc;
+    private double myXVelocity;
+    private double myYVelocity;
+    private double myXAcceleration;
+    private double myYAcceleration;
     private double myAngle;
-    private double[] myDirectionVec;
 
-    public MotionComponent(int velocity, int acceleration, int angle) {
-        myVel = velocity;
-        myAcc = acceleration;
-        myAngle = angle;
-        myDirectionVec = new double[2];
-        myDirectionVec[0] = Math.cos(Math.toRadians(angle));
-        myDirectionVec[1] = Math.sin(Math.toRadians(angle));
+    public MotionComponent(double xVelocity, double yVelocity, double xAcceleration, double yAcceleration, double angle) {
+        this.myXVelocity = xVelocity;
+        this.myYVelocity = yVelocity;
+        this.myXAcceleration = xAcceleration;
+        this.myYAcceleration = yAcceleration;
+        this.myAngle = angle;
     }
 
-    public double getVel() {
-        return myVel;
+    public double getXVelocity() {
+        return myXVelocity;
     }
 
-    public double getAcc() {
-        return myAcc;
+    public void setXVelocity(double xVelocity) {
+        this.myXVelocity = xVelocity;
+    }
+
+    public double getYVelocity() {
+        return myYVelocity;
+    }
+
+    public void setYVelocity(double yVelocity) {
+        this.myYVelocity = yVelocity;
+    }
+
+    public double getXAcceleration() {
+        return myXAcceleration;
+    }
+
+    public void setXAcceleration(double xAcceleration) {
+        this.myXAcceleration = xAcceleration;
+    }
+
+    public double getYAcceleration() {
+        return myYAcceleration;
+    }
+
+    public void setYAcceleration(double yAcceleration) {
+        this.myYAcceleration = yAcceleration;
     }
 
     public double getAngle() {
         return myAngle;
     }
 
-    public void setVel(double vel) {
-        this.myVel = vel;
-    }
-
-    public void setAcc(double acc) {
-        this.myAcc = acc;
-    }
-
     public void setAngle(double angle) {
         this.myAngle = angle;
     }
-
-    public void setDirectionVec(double[] directionVec) {
-        this.myDirectionVec = directionVec;
-    }
-
-    public double[] getDirectionVec() { return myDirectionVec; }
 }
