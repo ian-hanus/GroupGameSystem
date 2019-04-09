cf290d7d1bf7a1309e7baf2fbe4429fad05a4e32
Ian Hanus <ih52@duke.edu>
2019-04-09 19:39:01 -0400

    Added protected getters to Level

--- src/Engine/src/DataManager/Level.java
+++ src/Engine/src/DataManager/Level.java
@@ -3,6 +3,7 @@ package DataManager;
 import ECS.Components.Component;
 import Events.Event;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,13 +16,37 @@ public class Level {
     private Integer myLevelNumber;
     private List<Map<String, Component>> myObjectBank;
     private String myBackground;
+    private Integer myWidth;
+    private Integer myHeight;
 
     public Level(Integer levelNumber, Map<Integer, Map<String, Component>> activeObjects, List<Map<String, Component>> objectBank,
-                 Map<String[], Set<Event>[]> collisionResponses, String background){
+                 Map<String[], Set<Event>[]> collisionResponses, String background, int width, int height){
         myLevelNumber = levelNumber;
         myActiveObjects = activeObjects;
         myObjectBank = objectBank;
         myCollisionResponses = collisionResponses;
         myBackground = background;
+        myWidth = width;
+        myHeight = height;
+    }
+
+    public int getLevelNumber(){
+        return myLevelNumber;
+    }
+
+    public Map<Integer, Map<String, Component>> getActiveObjects(){
+        return Collections.unmodifiableMap(myActiveObjects);
+    }
+
+    public List<Map<String, Component>> getObjectBank(){
+        return Collections.unmodifiableList(myObjectBank);
+    }
+
+    public Map<String[], Set<Event>[]> getCollisionResponses(){
+        return Collections.unmodifiableMap(myCollisionResponses);
+    }
+
+    public String getBackground(){
+        return myBackground;
     }
 }
