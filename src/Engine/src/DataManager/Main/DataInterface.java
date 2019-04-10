c62121575f2beae921806ccda054b8fb2951c814
Hunter Gregory <hlg16@duke.edu>
2019-04-09 20:31:30 -0400

    continuing to merge from ians branch

---
+++ src/Engine/src/DataManager/Main/DataInterface.java
@@ -0,0 +1,7 @@
+package Main;
+
+public interface DataInterface {
+    void saveGame(String fileName, GameInformation gameInformation);
+
+    GameInformation loadGame(String fileName);
+}
--- src/Engine/src/module-info.java
+++ src/Engine/src/module-info.java
@@ -1,7 +1,14 @@
 module Engine {
     requires gson;
+<<<<<<< .merge_file_AApcXL
     exports controller;
     exports components;
     exports components.immutable_basic_component;
     exports parser;
+=======
+
+    exports EngineMain;
+    exports ECS;
+    exports ECS.Components;
+>>>>>>> .merge_file_ioC0Wt
 }
