738532dc8b493a8908c95fe0118a32e1d28b66ae
Carter Gay <cag65@duke.edu>
2019-04-09 20:26:04 -0400

    added Game Name field to GameStatusInfo HUD

--- src/Engine/src/DataManager/Main/GameInformation.java
+++ src/Engine/src/DataManager/Main/GameInformation.java
@@ -1,14 +1,16 @@
 package Main;
 
+import DataManager.Level;
+
 import java.util.List;
 import java.util.Map;
 
 public class GameInformation {
     private List<Level> myLevels;
     private Map<String, Integer> myScoreboard;
-    private Splash mySplash;
+    private Main.Splash mySplash;
 
-    public GameInformation(List<Level> levels, Map<String, Integer> scoreboard, Splash splash){
+    public GameInformation(List<Level> levels, Map<String, Integer> scoreboard, Main.Splash splash){
         myLevels = levels;
         myScoreboard = scoreboard;
         mySplash = splash;
