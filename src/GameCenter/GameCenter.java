feda64322c792d2ce7f5e55d96b3e7b06a1b2b7b
Januario Carreiro <jjc70@duke.edu>
2019-04-15 01:54:53 -0400

    Added JavaDoc to GameCenter, minor changes to GameCenter MVC classes wrt scrollpane for thumbnails

--- src/GameCenter/GameCenter.java
+++ src/GameCenter/GameCenter.java
@@ -1,32 +1,64 @@
 package GameCenter;
 
+import GameCenter.GameData.DataParser;
+import GameCenter.GameData.DataStruct;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * GameCenter.java is the launcher for the Game Center, where a user can launch games through the Player, launch the
+ * Authoring Environment to edit games, do several actions to the game, and interact with other users through the
+ * social hub.
+ *
+ * GameCenter.java works in conjunction with GameCenter.fxml and GameCenter.css, both found in the resources folder, as
+ * well as GameCenterController.
+ *
+ * GameCenter.java, GameCenter.fxml & GameCenter.css, and GameCenterController.java are the model, view, and controller,
+ * respectively. Keep this in mind when refactoring/writing new code.
+ *
+ * TODO: Finish refactoring code from GCScreen to GameCenter
+ * TODO: Figure out how to use custom fonts in .fxml or .css
+ * TODO: Implement Social Hub
+ * TODO: Add login items/user picture in conjunction with the data team
+ *
+ * @author Januario Carreiro
+ * 14 April 2019
+ */
 public class GameCenter extends Application {
-    public static Font sofiaPro, sofiaProSmall, bebasKai, bebasKaiMedium;
+    /**
+     * Method to launch the application. Required when extending Application.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     /**
+     * Method to set up the stage. Required when extending Application.
      *
-     * @param stage
-     * @throws Exception
+     * @param stage window for the Game Center.
+     * @throws Exception if resources could not be found, which should never happen.
      */
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(this.getClass().getResource("/GameCenter.fxml"));
 
-        try {
-            sofiaPro = Font.loadFont(this.getClass().getResourceAsStream("/fonts/sofiapro-light.otf"),30);
-            sofiaProSmall = Font.loadFont(this.getClass().getResourceAsStream("/fonts/sofiapro-light.otf"),15);
-            bebasKai = Font.loadFont(this.getClass().getResourceAsStream("/fonts/bebaskai.otf"),15);
-            bebasKaiMedium = Font.loadFont(this.getClass().getResourceAsStream("/fonts/bebaskai.otf"),25);
-        } catch (Exception e) {
-            System.out.println("A problem occurred when loading fonts.");
-        }
+//        try {
+//            sofiaPro = Font.loadFont(this.getClass().getResourceAsStream("/fonts/sofiapro-light.otf"),30);
+//            sofiaProSmall = Font.loadFont(this.getClass().getResourceAsStream("/fonts/sofiapro-light.otf"),15);
+//            bebasKai = Font.loadFont(this.getClass().getResourceAsStream("/fonts/bebaskai.otf"),15);
+//            bebasKaiMedium = Font.loadFont(this.getClass().getResourceAsStream("/fonts/bebaskai.otf"),25);
+//        } catch (Exception e) {
+//            System.out.println("A problem occurred when loading resources.");
+//        }
 
         Scene scene = new Scene(root, 975, 500);
         scene.getStylesheets().add("GameCenter.css");
@@ -34,13 +66,5 @@ public class GameCenter extends Application {
         stage.setTitle("Game Center");
         stage.setScene(scene);
         stage.show();
-    }
-
-    /**
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        launch(args);
     }
 }
--- src/GameCenter/GameCenterController.java
+++ src/GameCenter/GameCenterController.java
@@ -1,15 +1,21 @@
 package GameCenter;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 /**
  * TODO: Write JavaDoc
  */
 public class GameCenterController {
-//    @FXML
-//    private Text titleText;
+    public Pane socialPane;
+    public VBox thumbPaneContent;
+    public Pane descriptionPane;
+    public Text titleText;
+    public ScrollPane thumbPane;
 
 //    @FXML protected void selectGame(MouseEvent event) {
 //        Thumbnail thumbnail = (Thumbnail) event.getSource();
