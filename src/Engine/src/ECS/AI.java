627d59068544143c6126d4400f73ada307e989a7
DANIEL KINGSBURY <dpk14@duke.edu>
2019-04-21 00:19:34 -0400

    created aim, badAim, and goodAim methods.

--- src/Engine/src/ECS/AI.java
+++ src/Engine/src/ECS/AI.java
@@ -1,5 +1,6 @@
 package Engine.src.ECS;
 
+import Engine.src.Components.AimComponent;
 import Engine.src.Components.BasicComponent;
 import Engine.src.Components.LOSComponent;
 import Engine.src.Components.MotionComponent;
@@ -8,41 +9,40 @@ import java.awt.*;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class AI {
 
     private EntityManager myEntityManager;
 
-    public AI(EntityManager entityManager){
+    public AI(EntityManager entityManager) {
         myEntityManager = entityManager;
     }
 
-    public void follow(int referenceID, int targetID){
+    public void follow(int referenceID, int targetID) {
         LOSComponent LOSComp = myEntityManager.getComponent(referenceID, LOSComponent.class);
-        double [] distance = findDistanceVector(referenceID, targetID);
-        double deltaX = distance[0];
-        double deltaY = distance[1];
-        double magnitude = Math.pow(Math.pow(deltaX, 2) + Math.pow(deltaY, 2), .5);
+        double[] distanceVec = findDistanceVector(referenceID, targetID);
+        double magnitude = calculateMagnitude(distanceVec);
         if (LOSComp == null || isInLOS(targetID, referenceID, magnitude, LOSComp.getLOS())) {
-            myEntityManager.moveInDirection(referenceID, findDirection(distance));
+            myEntityManager.moveInDirection(referenceID, findDirection(distanceVec));
         }
     }
 
-    public void patrol(int entityID, ArrayList<Point2D> patrolRoute){
+    public void patrol(int entityID, ArrayList<Point2D> patrolRoute) {
         int patrolStage = findPatrolStage(entityID, patrolRoute);
         BasicComponent basic = myEntityManager.getComponent(entityID, BasicComponent.class);
         double[] distance = findDistanceVector(new Point2D.Double(basic.getX(), basic.getY()), patrolRoute.get(patrolStage));
         myEntityManager.moveInDirection(entityID, findDirection(distance));
     }
 
-    private int findPatrolStage(int entityID, ArrayList<Point2D> patrolRoute){
+    private int findPatrolStage(int entityID, ArrayList<Point2D> patrolRoute) {
         BasicComponent basic = myEntityManager.getComponent(entityID, BasicComponent.class);
         double[] topLeftCorner = {basic.getX(), basic.getY()};
         double[] bottomRightCorner = {basic.getX() + basic.getWidth(), basic.getY() + basic.getHeight()};
         Point2D earlierPoint;
         Point2D nextPoint;
 
-        for (int currentPatrolPathIndex = 0; currentPatrolPathIndex < patrolRoute.size(); currentPatrolPathIndex++){
+        for (int currentPatrolPathIndex = 0; currentPatrolPathIndex < patrolRoute.size(); currentPatrolPathIndex++) {
             earlierPoint = patrolRoute.get(currentPatrolPathIndex);
             nextPoint = patrolRoute.get(currentPatrolPathIndex + 1);
             if (Line2D.linesIntersect(earlierPoint.getX(), earlierPoint.getY(), nextPoint.getX(), nextPoint.getY(),
@@ -57,7 +57,7 @@ public class AI {
         return (LOS > distance && !myEntityManager.obscured(targetID, referenceID));
     }
 
-    private double[] findDistanceVector(int referenceID, int targetID){
+    private double[] findDistanceVector(int referenceID, int targetID) {
         BasicComponent referenceBasic = myEntityManager.getComponent(referenceID, BasicComponent.class);
         BasicComponent targetBasic = myEntityManager.getComponent(targetID, BasicComponent.class);
         double deltaX = targetBasic.getX() - referenceBasic.getX();
@@ -66,18 +66,53 @@ public class AI {
         return vector;
     }
 
-    private double[] findDistanceVector(Point2D reference, Point2D target){
+    private double[] findDistanceVector(Point2D reference, Point2D target) {
         double deltaX = reference.getX() - reference.getX();
         double deltaY = target.getY() - target.getY();
         double[] vector = {deltaX, deltaY};
         return vector;
     }
 
-    private double[] findDirection(double[] vector){
+    private double[] findDirection(double[] vector) {
         double deltaX = vector[0];
         double deltaY = vector[1];
-        double magnitude = Math.pow(Math.pow(deltaX, 2) + Math.pow(deltaY, 2), .5);
-        double [] direction = {deltaX / magnitude, deltaY / magnitude};
+        double magnitude = calculateMagnitude(vector);
+        double[] direction = {deltaX / magnitude, deltaY / magnitude};
         return direction;
+    }
+
+    private double calculateMagnitude(double[] vector) {
+        double deltaX = vector[0];
+        double deltaY = vector[1];
+        return Math.pow(Math.pow(deltaX, 2) + Math.pow(deltaY, 2), .5);
+    }
+
+    public void badAim(int shooterID, int targetID, double accuracy){
+        double[] distanceVec = findDistanceVector(shooterID, targetID);
+        double angle = Math.atan(distanceVec[1] / distanceVec[0]);
+        aim(shooterID, angle, accuracy);
+    }
+
+    public void goodAim(int shooterID, int targetID, double accuracy){
+        MotionComponent motion = myEntityManager.getComponent(targetID, MotionComponent.class);
+        if (motion == null) aimPoorly(shooterID, targetID, accuracy);
+        else {
+            double xVel = motion.getXVelocity();
+            double yVel = motion.getYVelocity();
+            double[] distanceVec = findDistanceVector(shooterID, targetID);
+            double idealX = distanceVec[0] + (myEntityManager.getStepTime() * xVel);
+            double idealY = distanceVec[1] + (myEntityManager.getStepTime() * yVel);
+            double[] idealDistanceVec = {idealX, idealY};
+            double angle = Math.atan(idealDistanceVec[1] / idealDistanceVec[0]);
+            aim(shooterID, angle, accuracy);
+        }
+    }
+
+    private void aim(int shooterID, double angle, double accuracy){
+        Random rand = new Random();
+        angle += rand.nextGaussian() * angle * (1 - accuracy);
+        AimComponent aim = myEntityManager.getComponent(shooterID, AimComponent.class);
+        aim.setXAim(Math.cos(angle));
+        aim.setYAim(Math.sin(angle));
     }
 }
