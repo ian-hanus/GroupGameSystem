a51b370061870a5e4334bdf9b2deaa3341bb12af
Carter Gay <cag65@duke.edu>
2019-04-22 00:17:09 -0400

    added comments for DataTracker and PlotBuilder

--- src/Player/src/GameStats/DataTracker.java
+++ src/Player/src/GameStats/DataTracker.java
@@ -1,18 +1,29 @@
 package Player.src.GameStats;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
+/**
+ * This class is used to store data for a desired numerical variable
+ * @author Carter Gay
+ */
 public class DataTracker {
 
     private ArrayList<Double> myData;
     private String dataName;
 
+    /**
+     * DataTracker constructor where the name identifies the variable stored
+     * @param name
+     */
     public DataTracker(String name) {
         dataName = name;
         myData = new ArrayList<>();
     }
 
+    /**
+     * Add a data point to the List
+     * @param x
+     */
     public void storeData(double x) {
             myData.add(x);
     }
@@ -21,10 +32,18 @@ public class DataTracker {
 
     }
 
+    /**
+     * Get the name of the data being stored
+     * @return
+     */
     public String getDataName() {
         return dataName;
     }
 
+    /**
+     * Get the data values
+     * @return
+     */
     public ArrayList<Double> getData() {
         return myData;
     }
